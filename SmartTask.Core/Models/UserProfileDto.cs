2b80dc2af1e44aa1f321264c66cd25fa7038eefe
hussein22sayed <168219467+hussein22sayed@users.noreply.github.com>
2025-05-03 01:50:59 +0300

    Create UserProfileDto.cs

---
+++ SmartTask.Core/Models/UserProfileDto.cs
@@ -0,0 +1,9 @@
+namespace SmartTask.Core.Models
+{
+    public class UserProfileDto
+    {
+        public int Id { get; set; }      // User ID
+        public string Name { get; set; } // User name
+        public string Email { get; set; } // User email
+    }
+}
