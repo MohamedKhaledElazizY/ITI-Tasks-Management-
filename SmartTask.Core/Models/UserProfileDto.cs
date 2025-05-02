540c6d29cab8975c3baddfa9c1f165c425c8af6f
LoayAlkashif <loayalkashif@gmail.com>
2025-05-03 02:47:20 +0300

    Revert "Create UserProfileDto.cs":wq
    This reverts commit a7986479e127ef1a64bd3eb65bc970a8a09150d7.

--- SmartTask.Core/Models/UserProfileDto.cs
+++
@@ -1,9 +0,0 @@
-namespace SmartTask.Core.Models
-{
-    public class UserProfileDto
-    {
-        public int Id { get; set; }      // User ID
-        public string Name { get; set; } // User name
-        public string Email { get; set; } // User email
-    }
-}
