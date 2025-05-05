e92b80e212301d3531c4e044bfb643aaf454dd8d
Hamza Elsharkawy <hamzaelsharkawy17@gmail.com>
2025-05-05 23:18:02 +0300

    TaskController

---
+++ SmartTask.Web/Controllers/TaskController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartTask.Core.Models;
+using SmartTask.DataAccess.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartTask.Web.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly SmartTaskContext _context;
+
+        public TaskController(SmartTaskContext context)
+        {
+            _context = context;
+        }
+
+        // الأكشن الأول: هيجيب التاسكات الخاصة بيوزر معين في مشروع محدد
+        public async Task<IActionResult> TasksForUserInProject(int projectId, string userId)
+        {
+            var tasks = await _context.Tasks
+                .Where(t => t.ProjectId == projectId && t.AssignedToId == userId)
+                .ToListAsync();
+
+            return View(tasks);
+        }
+
+        // الأكشن الثاني: هيجيب كل التاسكات الخاصة بمشروع معين
+        public async Task<IActionResult> TasksForProject(int projectId)
+        {
+            var tasks = await _context.Tasks
+                .Where(t => t.ProjectId == projectId)
+                .ToListAsync();
+
+            return View(tasks);
+        }
+
+        // الأكشن الثاني: هيجيب كل التاسكات الخاصة بيوزر معين
+        public async Task<IActionResult> TasksForUser(string userid)
+        {
+            var tasks = await _context.Tasks
+                .Where(t => t.AssignedToId == userid)
+                .ToListAsync();
+
+            return View(tasks);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
