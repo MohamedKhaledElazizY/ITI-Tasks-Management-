2bebb583b30d040c75c8c2440a5d64a846a60acf
AHMADMOSTAFAA <ahmadmostafaa02@gmail.com>
2025-05-23 23:24:51 +0300

    AdminController

---
+++ SmartTask.Web/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartTask.Core.IRepositories;
+using SmartTask.DataAccess.Repositories;
+
+namespace SmartTask.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+
+        public AdminController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public IActionResult Index(string id)
+        {
+          
+        var User =_userRepository.GetByIdAsync(id);
+            if (User == null)
+            {
+                return NotFound();
+            }
+            return View(User);
+        }
+    }
+}
