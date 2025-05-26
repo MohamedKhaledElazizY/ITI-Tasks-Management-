d32c100af07787297f2bcbbc3bfa6ab9948394ad
AHMADMOSTAFAA <ahmadmostafaa02@gmail.com>
2025-05-26 12:58:54 +0300

    Admin and Employee Dashboard

---
+++ SmartTask.Web/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SmartTask.Core.IRepositories;
+
+namespace SmartTask.Web.Controllers
+{
+    [Authorize]
+    public class EmployeeController : Controller
+    {
+
+        private readonly IUserRepository _userRepository;
+        public EmployeeController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public async Task<IActionResult> Index(string id)
+        {
+
+            var User = await _userRepository.GetWithDetailsAsync(id);
+            if (User == null)
+            {
+                return NotFound();
+            }
+            return View(User);
+        }
+    }
+    
+}
