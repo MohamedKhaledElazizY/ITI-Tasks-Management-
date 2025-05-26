5af78ae07a98e1c6e458dd1d40ff9cac975e5d2b
Marwa Sayed <Marwa20136@fci.bu.edu.eg>
2025-05-26 19:10:45 +0300

    Update user edit and details views

--- SmartTask.Web/Controllers/ManagerController.cs
+++ SmartTask.Web/Controllers/ManagerController.cs
@@ -14,6 +14,7 @@ namespace SmartTask.Web.Controllers
 
         public ManagerController(
             SmartTaskContext context,
+
             UserManager<ApplicationUser> userManager)
         {
             _context = context;
