bae13ef6e2ef8ba301b2d0d98705bc70bb9cf46b
AHMADMOSTAFAA <ahmadmostafaa02@gmail.com>
2025-06-01 02:04:01 +0300

    New main

--- SmartTask.DataAccess/Migrations/20250531205215_initial create.cs
+++ SmartTask.DataAccess/Migrations/20250531205215_initial create.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace SmartTask.DataAccess.Migrations
 {
     /// <inheritdoc />
-    public partial class init : Migration
+    public partial class initialcreate : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
